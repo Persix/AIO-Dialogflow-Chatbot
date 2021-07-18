27c5661c0b219887bfe21e23b51de0f8939a0f99
Persix <105737365+Persix@users.noreply.github.com>
2021-07-18 16:37:30 +0000

    Update from Forestry.io Zoltán Bába updated deploy.md

--- service.js
+++ service.js
@@ -6,4 +6,6 @@ const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser');
-app.use(express.urlen
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('trust prox
