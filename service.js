82e4f5945acbe81359d3a2cc9c31d10f228f9027
Persix <105737365+Persix@users.noreply.github.com>
2022-01-02 20:19:44 +0000

    fixed wrong merge

--- service.js
+++ service.js
@@ -137,4 +137,5 @@ app.post('/webhook', async(request, response) => {
         console.log("Validating the requested appointment date with fulfillment service...")
         var time_parameter = parameters["time"][0]
         var date_parameter = parameters["date"][0]
-        var
+        var date = moment(date_parameter).format('M/DD/YYYY')
+        var formatted_date = momen
