bac8f587bb9d29fd26b8acfd7c9b4fa7c7044d4a
Persix <105737365+Persix@users.noreply.github.com>
2022-04-12 22:27:41 +0000

    Fix unit test

--- service.js
+++ service.js
@@ -184,4 +184,4 @@ app.post('/webhook', async(request, response) => {
                 if (context_name == session_path + '/contexts/' + 'appointment-booking-reschedule') {
                     console.log("Customer is in the process of rescheduling an appointment...") 
                     console.log("Attempting to update appointment details to spreadsheet...")
- 
+                    var update_sheet = await ChangeAppointment(pa
