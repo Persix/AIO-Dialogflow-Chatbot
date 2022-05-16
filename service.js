22c7717a92072e5eec8a85fca6963d53eda45b42
Persix <105737365+Persix@users.noreply.github.com>
2022-05-16 06:14:02 +0000

    Fix readme format

--- service.js
+++ service.js
@@ -222,4 +222,5 @@ app.post('/webhook', async(request, response) => {
         // ******* The name of a specific context object has the format below:  ******** //
         
         console.log("Using context to determine whether the customer intends to book, reschedule, or cancel their appointment\n")
-        // Strategy: Parse the most recent context object in the query request and apply the following operations
+        // Strategy: Parse the most recent context object in the query request and apply the following operations:
+        // Step (1) Split the context name by the '/' backslash c
