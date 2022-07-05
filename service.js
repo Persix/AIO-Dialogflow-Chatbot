a89e07e14b83538a4c47a1910e1b19fbd8be6a88
Persix <105737365+Persix@users.noreply.github.com>
2022-07-05 09:06:54 +0000

    Fix url

--- service.js
+++ service.js
@@ -300,4 +300,7 @@ app.post('/webhook', async(request, response) => {
                     console.log(`${FirstName} ${LastName}`)
                     const context_cancel = {
                         "entity": "name",
-                        "acti
+                        "action": "cancel"
+                        }
+                        
+               
