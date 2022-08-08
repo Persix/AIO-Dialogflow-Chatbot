6879e157d4ca24361e1e128bc44a9c2676b41a51
Persix <105737365+Persix@users.noreply.github.com>
2022-08-08 18:22:20 +0000

    revert this

--- service.js
+++ service.js
@@ -349,4 +349,6 @@ app.post('/webhook', async(request, response) => {
                 console.log("Saved customer's email to session cache...")
             })
             var jsonResponse = {
-                "fulfillmentText" : "Thanks! I need your phone number in order to book the appointment
+                "fulfillmentText" : "Thanks! I need your phone number in order to book the appointment, we're almost finished!"
+            }
+            console.log("Activ
