57e00430273d6efa6ac40bef42edde3e62401fcc
Persix <105737365+Persix@users.noreply.github.com>
2022-10-08 05:39:14 +0000

    enable travis ci

--- service.js
+++ service.js
@@ -452,4 +452,5 @@ app.post('/webhook', async(request, response) => {
                             var jsonResponse  = {
                                 "fulfillmentText" : `Perfect! I just looked up the address, it's only ${distance} away, which meets the fundraiser requirements. How many bags do you plan on selling?`
                             }
-                            console.log(
+                            console.log("Attempting to store DestinationAddress in memory...")
+                        
