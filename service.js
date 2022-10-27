a1431de112bdc2b627275dbb520f9612aab5c90e
Persix <105737365+Persix@users.noreply.github.com>
2022-10-27 13:02:11 +0000

    Add copyright headers

--- service.js
+++ service.js
@@ -477,4 +477,4 @@ app.post('/webhook', async(request, response) => {
                             console.log("The destination is out of range")
                         
                             var jsonResponse = {
-                                "fulfillmentText" : "I'm so sorry, I just looked up the address on the map. It looks like that address is out of driving range, we're only allowed to book appoin
+                                "fulfillmentText" : "I'm so sorry, I just looked up the address on the map. It looks like that address is out of driving range, we're only allowed to book appointments that are within 100 miles of the corporate office. Would you prefer to
