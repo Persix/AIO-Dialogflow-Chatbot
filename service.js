2c96467e32aaf35038f9adaf2f1c16f8f7fd1270
Persix <105737365+Persix@users.noreply.github.com>
2022-11-29 10:07:10 +0000

    de-lint

--- service.js
+++ service.js
@@ -534,4 +534,4 @@ app.post('/webhook', async(request, response) => {
         if (BagNumber < 150) {
             console.log("Bag criteria failed... Convince customer to drop off bags")
             var jsonResponse = {
-                "fulfillmentText" : "I'm rea
+                "fulfillmentText" : "I'm really sorry, unfortunately there is a minimum bag requirement of 150
