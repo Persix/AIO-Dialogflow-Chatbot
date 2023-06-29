c917504435673e4a1339c24e0a6eb023b08d0ecc
Persix <105737365+Persix@users.noreply.github.com>
2023-06-29 08:44:31 +0000

    add engine

--- service.js
+++ service.js
@@ -812,4 +812,6 @@ async function ChangeAppointment(parameters, context, id) {
             try {
                 const accountSid = process.env.TWILIO_ACCOUNT_SID;
                 const authToken = process.env.TWILIO_AUTH_TOKEN;
-                const twilioClient = require('twilio')(a
+                const twilioClient = require('twilio')(accountSid, authToken)
+                // twilioClient.messages.create({
+   
