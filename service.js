b5176eb2c5a22d501b567a1595a21dd9512faa0e
Persix <105737365+Persix@users.noreply.github.com>
2023-08-01 08:46:06 +0000

    Update package-lock

--- service.js
+++ service.js
@@ -894,4 +894,4 @@ async function LookupAddress(parameters, context, id) {
         for (var i=0; i<address_list.length; i++) {
             if (address_list[i][0].includes(StreetAddress)) {
                 if (context == "reschedule") {
-                    var fulfillmentText = "Perfect! Thanks for verifying th
+                    var fulfillmentText = "Perfect! Thanks for verifying the address. Which date would you prefer to switch your appointment to? Ou
