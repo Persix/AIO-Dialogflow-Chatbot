8237934d7508bc2bd65714d14523b62205733728
Persix <105737365+Persix@users.noreply.github.com>
2023-08-25 19:10:57 +0000

    Updated Mongolian translation

--- service.js
+++ service.js
@@ -934,4 +934,5 @@ async function LookupAddress(parameters, context, id) {
 
                     if (message == "success") {
 
-                        var jsonResponse = "Perfect! Thank you for being so patient, I just cancelled your appointment. Would you mind providing feedback on how I could improve? I want to 
+                        var jsonResponse = "Perfect! Thank you for being so patient, I just cancelled your appointment. Would you mind providing feedback on how I could improve? I want to learn what I can to better so that I can be more helpful in the future."
+                   
