3193738a44ab30400679651c367314ed8f4e9faf
Persix <105737365+Persix@users.noreply.github.com>
2023-10-04 17:53:36 +0000

    added admin

--- service.js
+++ service.js
@@ -1036,4 +1036,5 @@ async function CreateAppointment(session_id) {
     try {
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
-    const twili
+    const twilioClient = require('twilio')(accountSid, authToken)
+    twilioClient.mes
